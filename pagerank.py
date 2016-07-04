3ceb97bbe8da5db290506b71d181542fc7b9f1ab
Mengxiong <mliu60@illinois.edu>
2016-07-04 15:52:57 -0500

    rank

---
+++ pagerank.py
@@ -0,0 +1,38 @@
+from neo4j.v1 import GraphDatabase, basic_auth
+
+
+driver = GraphDatabase.driver("bolt://localhost:7687", auth = basic_auth("neo4j", "WCup20140613"))
+session = driver.session()
+
+initilize =
+	"""
+	match (a) set a.pagerank = 0.0
+	"""
+
+iterate =
+	"""
+	with collect(distinct a) as nodes,count(a) as num_nodes
+	unwind nodes as a
+	match (a)-[r]-(b)
+	with a,collect(r) as rels, count(r) as num_rels,1.0/num_nodes as rank
+		unwind rels as rel
+	   	set endnode(rel).pagerank =
+			case
+				when num_rels > 0 and id(startnode(rel)) = id(a) then
+					endnode(rel).pagerank + rank/(num_rels)
+				else endnode(rel).pagerank
+			end
+		,
+		startnode(rel).pagerank =
+			case
+				when num_rels > 0 and id(endnode(rel)) = id(a) then
+					startnode(rel).pagerank + rank/(num_rels)
+				else startnode(rel).pagerank
+			end
+	"""
+
+# session.run(initilize)
+for i in range(1):
+	session.run(iterate)
+
+session.close()
