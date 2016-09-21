9ded376f9043df03d8e659bc87f87b71d848b1ae
Mengxiong Liu <mengxiongliu@Mengxiongs-MacBook-Pro.local>
2016-09-20 22:59:26 -0500

    weighted graph

--- doc2vec.py
+++ doc2vec.py
@@ -4,6 +4,7 @@ import argparse
 import numpy as np
 from gensim.models import Doc2Vec
 from gensim.models.doc2vec import LabeledSentence
+from scipy.spatial.distance import cosine
 from nltk import word_tokenize, pos_tag
 from neo4j.v1 import GraphDatabase, basic_auth
 
@@ -13,15 +14,13 @@ valid_POS = set(['NN', 'NNP', 'NNS', 'NNPS', 'JJ', 'JJR', 'JJS'])
 def parse_args():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--title', nargs='?', default='title.txt')
-	parser.add_argument('--phrase', nargs='?', default='phrase.txt')
 	parser.add_argument('--vector', nargs='?', default='doc2vec.txt')
 	return parser.parse_args()
 
 
 def extract_phrases(text):
-	print "Extracting phrases"
 	token_buffer = []
-	tokens = word_tokenize(text)
+	tokens = word_tokenize(text.lower())
 	annotation = pos_tag(tokens)
 	for (token, pos) in annotation:
 		if pos in valid_POS:
@@ -36,38 +35,29 @@ def query_papers(session):
 			lower = i * epoch
 			upper = (i + 1) * epoch
 			for title in list(session.run("match (p:Paper) where ID(p) >= %d and ID(p) < %d return ID(p) as ID, p.title as title" % (lower, upper))):
-				f.write(str(title['ID']) + ', ' + title['title'] + '\n')
+				f.write(str(title['ID']) + ', ' + u' '.join(extract_phrases(title['title'])) + '\n')
 
 		for i in xrange(num_node):
 			title_buffer = ""
-			for title in list(session.run("match (n)--(p:Paper) where ID(n) = %d and (n:Researcher or n:Conference) return p.title as title" % i)):
-				title_buffer += (title['title'] + ' ')
+			for title in list(session.run("match (n)--(p:Paper) where ID(n) = %d and (n:Researcher or n:Conference) and p.year >= 2010 return p.title as title" % i)):
+				title_buffer += (u' '.join(extract_phrases(title['title'])) + ' ')
 			if title_buffer != "":
 				f.write(str(i) + ', ' + title_buffer + '\n')
+				
 	f.close()
-
-def convert_to_phrases():
-	with io.open(args.title, 'r', encoding = 'utf-16') as fr:
-		with io.open(args.phrase, 'w', encoding = 'utf-16') as fw:
-			for line in fr:
-				line = line[:-1].split(', ', 1)
-				phrases = extract_phrases(line[1])
-				fw.write(line[0] + ', ' + ' '.join(phrases) + '\n')
-		fw.close()
-	fr.close()
 
 def learn_vectors():
 	print "Learning embeddings"
 	document = []
 	labels = []
 	idx = 0
-	with io.open(args.phrase, 'r', encoding = 'utf-16') as f:
+	with io.open(args.title, 'r', encoding = 'utf-16') as f:
 		for line in f:
 			document.append(LabeledSentence(line.split(', ')[1], [idx]))
 			labels.append(line.split(', ')[0])
 			idx += 1
 	f.close()
-	model = Doc2Vec(document, size=64, window=8, min_count=1, workers=4, iter=20)
+	model = Doc2Vec(document, size=64, window=8, min_count=4, workers=8, iter=32)
 	with open(args.vector, 'w') as f:
 		for i in xrange(len(labels)):
 			f.write(str(labels[i]) + ', ' + ' '.join(map(str, model.docvecs[i])) + '\n')
@@ -80,6 +70,16 @@ def insert_vectors(session):
 			pair = line[:-1].split(', ', 1)
 			session.run("match (n) where ID(n) = %d set n.doc2vec = '%s'" % (int(pair[0]), pair[1]))
 	f.close()
+
+def assign_weight(session):
+	print "Assigning weight to relationships"
+	num_node = session.run("match (n) return count(*) as count").single()['count']
+	for i in xrange(num_node):
+		for edge in list(session.run("match (src)-[r]->(dest) where ID(src) = %d return ID(src) as srcID, ID(dest) as destID, src.doc2vec as srcVec, dest.doc2vec as destVec" % (i))):
+			srcID, destID = edge['srcID'], edge['destID']
+			srcVec, destVec = edge['srcVec'], edge['destVec']
+			cos = cosine(map(float, srcVec.split()), map(float, destVec.split()))
+			session.run("match (src)-[r]->(dest) set r.weight = %d" % cos)
 
 def aggregate_vectors(session):
 	print "Aggregating doc2vec"
@@ -102,9 +102,9 @@ session = driver.session()
 args = parse_args()
 
 query_papers(session)
-convert_to_phrases()
-# learn_vectors()
-# insert_vectors(session)
+learn_vectors()
+insert_vectors(session)
+assign_weight(session)
 
 session.close()
 
